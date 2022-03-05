79fde5a583e857c1fc31c3daeeb34e70477b9a3e
Benjamin Demaziere <benjamin.demaziere@viseo.com>
2022-03-05 15:12:20 +0100

    first implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(),FormsModule, ReactiveFormsModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAnalyticsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
